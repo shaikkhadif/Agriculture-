f030418e99e86b5a85c835ab38fe10b7a2b07838
shaikkhadif <khadeef143@gmail.com>
2024-11-29 22:00:09 +0530

    initail

---
+++ js/myjava.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var disclaimer = document.querySelector("img[alt='www.000webhost.com']");
+    if(disclaimer) {
+        disclaimer.remove();
+    }
+});
